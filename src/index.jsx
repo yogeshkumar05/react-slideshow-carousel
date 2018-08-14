7ff063aa15997b90154a7d46d9a7bf7607fc038c
Yogesh <yogeshkumar.05@live.in>
2018-08-14 11:54:45 +0530

    added option to pass configs to Carousel component

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import mockData from '../data/mockData.json';
 import Main from './components/Main';
 import '../styles/index.scss'
-const MyComponent = () => (
-    <Main/>
-);
+const MyComponent = (props) => {
+    const config = props.config || {
+        "data": mockData.images, 
+        "infinite": true, 
+        "tofro" : false, 
+        "autoplay": true, 
+        "idleTime" : 2000,
+        "showCaption": true
+    };
+    return <Main config={config}/>
+};
 export default MyComponent;
